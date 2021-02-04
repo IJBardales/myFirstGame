0837a53784cab918f4ba438957a5abd276966cd9
IJBardales <ingrid.bardales@accenturefederal.com>
2021-02-04 15:44:57 -0500

    saved weapons.js

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,11 +1,14 @@
 import '../styles/index.scss';
 import { Config } from './config';
+import { Weapons } from './weapons';
 
 export class Game {
     gameBoardElement;
+    weapons;
 
     constructor(gameBoard) {
         this.gameBoardElement = gameBoard;
+        this.weapons = new Weapons();
     }
 
     run() {}
