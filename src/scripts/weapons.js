3b0d6d50abe30403d156ea4dc6d79696b945c06f
IJBardales <ingrid.bardales@accenturefederal.com>
2021-02-16 10:33:05 -0500

    Created weapons constructor in weapons.js but am getting error that module is not found

--- src/scripts/weapons.js
+++ src/scripts/weapons.js
@@ -1,1 +1,12 @@
-export class Weapons {}
+export class Weapons {
+    constructor(weapon){
+        this.weapon = weapon;
+    }
+    attack(){
+    let initialWeaponCount = 3;
+    return "Remember, you are only given "+ initialWeaponCount +" weapons, use them well!";
+    }
+}
+ 
+const sword = new Weapons("sword");
+console.log(sword.attack());
